975657bfa5903795206f7a6dd72c8d8687666b39
Arthur Kellermann <arthur.kellermann956@gmail.com>
2024-04-19 19:23:57 -0300

    fix: fix issue with db connection

--- PassIn.Api/Program.cs
+++ PassIn.Api/Program.cs
@@ -1,13 +1,22 @@
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using PassIn.Api.Filters;
+using PassIn.Application.UseCases.Attendees.GetAllByEventId;
+using PassIn.Application.UseCases.Attendees.RegisterAttendee;
+using PassIn.Application.UseCases.CheckIns.CheckInAttendee;
+using PassIn.Application.UseCases.Events.GetById;
 using PassIn.Application.UseCases.Events.Register;
 using PassIn.Application.Validators;
 using PassIn.Domain.Entities;
 using PassIn.Domain.Repositories;
 using PassIn.Domain.Repositories.Interfaces;
+using PassIn.Infrastructure.Database;
 using PassIn.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PassInDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
@@ -26,6 +35,10 @@ builder.Services.AddScoped<AbstractValidator<Attendee>, AttendeeValidator>();
 builder.Services.AddScoped<AbstractValidator<Event>, EventValidator>();
 
 builder.Services.AddScoped<RegisterEventUseCase>();
+builder.Services.AddScoped<RegisterAttendeeOnEventUseCase>();
+builder.Services.AddScoped<CheckInAttendeeUseCase>();
+builder.Services.AddScoped<GetAllAttendeesByEventIdUseCase>();
+builder.Services.AddScoped<GetEventByIdUseCase>();
 
 var app = builder.Build();
 
--- PassIn.Infrastructure/Database/PassInDbContext.cs
+++ PassIn.Infrastructure/Database/PassInDbContext.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using PassIn.Domain.Entities;
 
-namespace PassIn.Infrastructure.Database;
-public class PassInDbContext : DbContext
+namespace PassIn.Infrastructure.Database
 {
-    public DbSet<Event> Events { get; set; }
-    public DbSet<Attendee> Attendees { get; set; }
-    public DbSet<CheckIn> CheckIns { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public class PassInDbContext : DbContext
     {
-        optionsBuilder.UseSqlite("Data Source=\"D:\\OneDrive\\Documentos\\C#\\PassIn\\PassInDb.db\"");
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Attendee> Attendees { get; set; }
+        public DbSet<CheckIn> CheckIns { get; set; }
+
+        public PassInDbContext(DbContextOptions<PassInDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=\"D:\\OneDrive\\Documentos\\C#\\PassIn\\PassInDb.db\"");
+        }
     }
 }
